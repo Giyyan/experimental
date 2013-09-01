8c0dcb6332b1325f8f55aa6e445fd006a129f895
Giyyan <karbanovich.andrey@gmail.com>
2013-09-01 20:40:41 +0300

    add sudoku

--- work_calendar.py
+++ work_calendar.py
@@ -18,7 +18,7 @@ def count_day(start_date, wdays, days={0, 1, 2, 3, 4}):
     return week_range
 
 
-def get_count_day(first_day, work_days):
+def get_count_day(first_day_index, count_work_days):
     """
     Количество календарных дней начиная с дня недели(first_day) и имея work_days рабочих дней
     @first_day - int(0-6)
@@ -26,27 +26,27 @@ def get_count_day(first_day, work_days):
 
     @return int
     """
-    work_days = abs(work_days)
-    if first_day < 0:
-        first_day = 0
-    if first_day > 6:
-        first_day = 6
-    f_week = work_days / 5 + 1
-    if work_days + first_day > 5 and f_week == 1:
-        f_week = 2
-    week_range = (range(7) * f_week)[first_day:]
-    weekends = (7 - 5) * (f_week - 1)
-    if first_day > 4:
-        weekends -= 7 - first_day
-    last_day = len(week_range[first_day:]) - work_days - weekends
-    return len(week_range[first_day:-last_day])
+    count_work_days = abs(count_work_days)
+    if first_day_index < 0:
+        first_day_index = 0
+    if first_day_index > 6:
+        first_day_index = 6
+    full_week_count = count_work_days / 5 + 1
+    if count_work_days + first_day_index > 5 and full_week_count == 1:
+        full_week_count = 2
+    weekday_range = (range(7) * full_week_count)[first_day_index:]
+    weekends_count = (7 - 5) * (full_week_count - 1)
+    if first_day_index > 4:
+        weekends_count -= 7 - first_day_index
+    last_day_index = len(weekday_range[first_day_index:]) - count_work_days - weekends_count
+    return len(weekday_range[first_day_index:-last_day_index])
 
 
-def get_last_day(start_date, work_days):
+def get_last_day(start_date, count_work_days):
     """
-    Дату последнего дня после wdays будних дней
+    Дату последнего дня после count_work_days будних дней
     """
-    return start_date + datetime.timedelta(days=get_count_day(start_date.weekday(), work_days))
+    return start_date + datetime.timedelta(days=get_count_day(start_date.weekday(), count_work_days))
 
 
 def work_day_count(start_date, work_days, need_days={0, 1, 2, 3, 4}):
@@ -58,7 +58,7 @@ def work_day_count(start_date, work_days, need_days={0, 1, 2, 3, 4}):
     return start_date
 
 
-def _dedline_start(date, days):
+def _deadline_start(date, days):
     start_line = datetime.datetime.now(pytz.utc)
     if days and days > 0:
         start_date = datetime.datetime.strptime(date, "%Y-%m-%d").replace(tzinfo=pytz.utc)
@@ -82,8 +82,8 @@ print get_last_day(datetime.date(2013, 9, 2), 3)
 #print timeit.timeit('work_day_count(datetime.date(2013, 8, 1), 10000)', "from __main__ import work_day_count; import datetime", number=1)
 #print datetime.date(2013, 8, 1) + datetime.timedelta(days=len(count_day(datetime.date(2013, 8, 1), 3)))
 #print timeit.timeit('datetime.date(2013, 8, 1) + datetime.timedelta(days=len(count_day(datetime.date(2013, 8, 1), 10000)))', "from __main__ import count_day; import datetime", number=1)
-#print _dedline_start('2013-08-01', 3)
-#print timeit.timeit('_dedline_start("2013-08-01", 10000)', "from __main__ import _dedline_start; import datetime; import pytz", number=1)
+#print _deadline_start('2013-08-01', 3)
+#print timeit.timeit('_deadline_start("2013-08-01", 10000)', "from __main__ import _deadline_start; import datetime; import pytz", number=1)
 #print get_last_day(datetime.date(2013, 8, 1), 3)
 #print timeit.timeit('datetime.date(2013, 8, 1) + datetime.timedelta(days=len(count_selected_day(datetime.date(2013, 8, 1), 10000)))', "from __main__ import count_selected_day; import datetime", number=1)
 #print get_last_day(datetime.date(2013, 8, 1), 3)
